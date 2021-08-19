3160890a50e9ed16d390b4725c4691ce5c0e0643
di-shykh <di49@mail.ru>
2021-08-19 23:38:32 +0300

    task made

--- src/index.js
+++ src/index.js
@@ -39,6 +39,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let i=0;
+    let encodedLetters=[];
+
+    while (i<expr.length){
+        encodedLetters.push(expr.substr(i,10));
+        i+=10;
+    }
+    console.log(encodedLetters);
+    let MORSE_TABLE01={};
+    let tempStr;
+
+    for(let key in MORSE_TABLE){
+
+        tempStr=key.replaceAll('.','10');
+        tempStr=tempStr.replaceAll('-','11');
+
+        if(tempStr.length<10){
+            let arrLength=tempStr.length;
+            let tempArr=tempStr.split('');
+            while(tempArr.length<10){
+                tempArr.unshift('0');
+            }
+            tempStr=tempArr.join('');
+        } 
+        MORSE_TABLE01[tempStr]=MORSE_TABLE[key];
+    }
+    MORSE_TABLE01['**********']=' ';
+    console.log(MORSE_TABLE01);
+    let decodeArr=[];
+    decodeArr=encodedLetters.map(el=>MORSE_TABLE01[el]);
+    console.log(decodeArr);
+    return decodeArr.join('');
 }
 
 module.exports = {
