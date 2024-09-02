bea137e5518f968c83582e74eb1860d1a171469f
di-shykh <di49@mail.ru>
2024-09-02 11:41:36 +0300

    feat: add new solution of task

--- src/index.js
+++ src/index.js
@@ -1,76 +1,63 @@
 const MORSE_TABLE = {
-    '.-':     'a',
-    '-...':   'b',
-    '-.-.':   'c',
-    '-..':    'd',
-    '.':      'e',
-    '..-.':   'f',
-    '--.':    'g',
-    '....':   'h',
-    '..':     'i',
-    '.---':   'j',
-    '-.-':    'k',
-    '.-..':   'l',
-    '--':     'm',
-    '-.':     'n',
-    '---':    'o',
-    '.--.':   'p',
-    '--.-':   'q',
-    '.-.':    'r',
-    '...':    's',
-    '-':      't',
-    '..-':    'u',
-    '...-':   'v',
-    '.--':    'w',
-    '-..-':   'x',
-    '-.--':   'y',
-    '--..':   'z',
-    '.----':  '1',
-    '..---':  '2',
-    '...--':  '3',
-    '....-':  '4',
-    '.....':  '5',
-    '-....':  '6',
-    '--...':  '7',
-    '---..':  '8',
-    '----.':  '9',
-    '-----':  '0',
+    '.-': 'a',
+    '-...': 'b',
+    '-.-.': 'c',
+    '-..': 'd',
+    '.': 'e',
+    '..-.': 'f',
+    '--.': 'g',
+    '....': 'h',
+    '..': 'i',
+    '.---': 'j',
+    '-.-': 'k',
+    '.-..': 'l',
+    '--': 'm',
+    '-.': 'n',
+    '---': 'o',
+    '.--.': 'p',
+    '--.-': 'q',
+    '.-.': 'r',
+    '...': 's',
+    '-': 't',
+    '..-': 'u',
+    '...-': 'v',
+    '.--': 'w',
+    '-..-': 'x',
+    '-.--': 'y',
+    '--..': 'z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
 };
 
 function decode(expr) {
     // write your solution here
-    let i=0;
-    let encodedLetters=[];
-
-    while (i<expr.length){
-        encodedLetters.push(expr.substr(i,10));
-        i+=10;
+    let arrOfChars10 = [];
+    for (let i = 0; i < expr.length - 9; i += 10) {
+        arrOfChars10.push(expr.substr(i, 10));
     }
-    let MORSE_TABLE01={};
-    let tempStr;
-
-    for(let key in MORSE_TABLE){
-
-        tempStr=key.split(".").join("10");
-        tempStr=tempStr.split("-").join("11");
-
-        if(tempStr.length<10){
-            let arrLength=tempStr.length;
-            let tempArr=tempStr.split('');
-            while(tempArr.length<10){
-                tempArr.unshift('0');
+    let arrOfMorese = arrOfChars10.map(item => {
+        if (item === '**********') item = ' '
+        else {
+            let tempElement=item;
+            for (let j = 0; j < 5; j++) {
+                tempElement = tempElement.replace('00', '');
+                tempElement = tempElement.replace('10', '.');
+                tempElement = tempElement.replace('11', '-');
             }
-            tempStr=tempArr.join('');
-        } 
-        MORSE_TABLE01[tempStr]=MORSE_TABLE[key];
-    }
-    MORSE_TABLE01['**********']=' ';
-
-    let decodeArr=[];
-    decodeArr=encodedLetters.map(el=>MORSE_TABLE01[el]);
-    return decodeArr.join('');
+            item=tempElement;
+        }
+        return item;
+    })
+    return arrOfMorese.map(item => MORSE_TABLE[item] ? MORSE_TABLE[item] : ' ').join('');
 }
-
 module.exports = {
     decode
 }
